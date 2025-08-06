60561602d6b5e067c8f333d24a74058086a5df45
shee1234-cell <ss1760028@gmail.com>
2025-08-06 19:17:27 +0530

    17503

--- account/views.py
+++ account/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def menu_view(request):
+    menu = [
+        {
+            'name': 'Margherita Pizza'
+            'description': 'Classic cheese pizza with basil.'
+            'price': 299
+        },
+        {
+            'name': 'Paneer Tikka Pizza'
+            'description': 'Classic grilled paneer with freshly spicy chilli.'
+            'price': 299
+        },
+        {
+            'name': 'Chicken Pizza'
+            'description': 'Juciy chicken flavoured with Indian spices'
+            'price': 499
+        }
+    ]
+    return Response(menu)
+
+
