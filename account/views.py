a70950fa47a43073402d9f6c88613e12f7ed8981
shee1234-cell <ss1760028@gmail.com>
2025-08-06 19:44:01 +0530

    17505

--- account/views.py
+++ account/views.py
@@ -1,25 +1,9 @@
-from rest_framework import api_view
-from rest_framework.response import Response
-
-@api_view(['GET'])
-def menu_view(request):
-    menu = [
-        {
-            'name': 'Margherita Pizza'
-            'description': 'Classic cheese pizza with basil.'
-            'price': 299
-        },
-        {
-            'name': 'Paneer Tikka Pizza'
-            'description': 'Classic grilled paneer with freshly spicy chilli.'
-            'price': 299
-        },
-        {
-            'name': 'Chicken Pizza'
-            'description': 'Juciy chicken flavoured with Indian spices'
-            'price': 499
-        }
-    ]
-    return Response(menu)
-
-
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+   <meta charset ="UTF-8">
+   <title> Restaurant Menu </title>
+</head>
+<body>
+   <h1> Welcome to Our Restaurant! </h1>
+   <div>
