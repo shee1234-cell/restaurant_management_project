c0d18b551b4135b98424b6bd0ceff19581257834
shee1234-cell <ss1760028@gmail.com>
2025-08-20 11:35:13 +0530

    24018

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,5 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.conf import settings
+from django.shortcuts import render
+def home_view(request):
+    restaurant_name = settings.RESTAURANT_NAME 
+    return render(request ,'home.html',{'restaurant_name': restaurant_name})
