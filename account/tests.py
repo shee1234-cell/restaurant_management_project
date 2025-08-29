12857f019f7d17d1225a4e9996ec47467f864989
shee1234-cell <ss1760028@gmail.com>
2025-08-29 12:35:24 +0530

    36397

--- account/tests.py
+++ account/tests.py
@@ -1,6 +1,21 @@
 <!DOCKTYPE html>
 <html><head>
     <title> Contact Us {{restaurant_name}} </title>
+    <style>
+    body{
+        font-family: Arial, sans-serif:
+        text-align: center;
+        background-color: black;
+    }
+    h1{
+        color: red;
+        margin-top: 50px;
+    }
+    p{
+        font-size: 18px;
+        margin: 10px 0;
+    }
+    </style>
     </head>
 <body>
     <div class="contact-box">
