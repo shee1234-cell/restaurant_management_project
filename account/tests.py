d5c3c8e1a7e581baddced8e7eb6ed4230b2a75bb
shee1234-cell <ss1760028@gmail.com>
2025-09-02 19:31:04 +0530

    36396

--- account/tests.py
+++ account/tests.py
@@ -1,9 +1,10 @@
-<!DOCKTYPE html>
-<html><head>
-    <title> Contact Us {{restaurant_name}} </title>
-    <style>
-<body>
-     <h1> Welcome to {{ restaurant_name }} </h1>
-     <p>We are happy to serve you delicious meals.</p>
-</body>
-</html>
+from django.shortcuts import render
+
+def menu_list(request):
+    menu_items = [
+        {"name": "Pizza", "price": "250"},
+        {"name": "Pasta", "price": "350"},
+        {"name": "Biriyani", "price": "550"},
+        {"name": "Coffee", "price": "150"},
+    ]
+    return render(request, "menu_list.html", {"menu_item": menu_items})
